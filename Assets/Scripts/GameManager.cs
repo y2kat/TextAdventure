88dc53b28a508ea310f4125850b9718f0b380905
y2kat.ia <lentacomputadora@gmail.com>
2022-03-07 22:18:16 -0600

    commit readme

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private string[] palabrasGuardadas;
     [SerializeField] private string[] preguntas;
     [SerializeField] private string historia;
+
     [SerializeField] TextMeshProUGUI textoPreguntas;
     [SerializeField] TextMeshProUGUI textoHistoria;
     [SerializeField] TMP_InputField inputRespuesta;
     [SerializeField] GameObject botonRespuesta;
+    [SerializeField] GameObject botonReinicio;
 
     int indicePregunta = 0;
 
@@ -18,27 +21,19 @@ public class GameManager : MonoBehaviour
     {
       
         textoPreguntas.text = preguntas[indicePregunta];
-        //Debug.Log(preguntas.Length);
-        //Debug.Log(palabrasGuardadas.Length);
-        //palabrasGuardadas = new string[1];
-        //palabrasGuardadas[0] = preguntas[0];
         palabrasGuardadas = new string[preguntas.Length];
+
+        botonReinicio.SetActive(false);
 
     }
     public void GuardarRespuesta()
         {
-        //guardar respuesta del jugador
         palabrasGuardadas[indicePregunta] = inputRespuesta.text;
-        //limpiar texto para que el jugador pueda escribir de nuevo
         inputRespuesta.text = "";
-        //poner la siguiente pregunta
         indicePregunta++;
-        //TODO necesitamos comprobación
         if(indicePregunta >= preguntas.Length)
         {
-            //TODO si la comprobación es cierta ejecutar MostrarHistoria
             MostrarHistoria();
-            //return;
         }
         else
         {
@@ -48,12 +43,18 @@ public class GameManager : MonoBehaviour
 
     void MostrarHistoria()
     {
-        //TODO Mostrar un tmp que tenga toda la historia
         textoHistoria.gameObject.SetActive(true);
-        textoHistoria.text = historia;
-        //TODO ocultar elementos que no se utilizan
+        textoHistoria.text = string.Format(historia, palabrasGuardadas);
+        botonReinicio.SetActive(true);
+
         textoPreguntas.gameObject.SetActive(false);
         botonRespuesta.SetActive(false);
         inputRespuesta.gameObject.SetActive(false);
+    }
+
+    public void ReiniciarJuego()
+    {
+        int indexEscena = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(indexEscena);
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Text Adventure es un juego creado en Unity3D para explicar el uso de TextMeshPro y bases de Anchors de Unity.
 
+Para probar el juego ingresa [aquí] (https://y2katia.itch.io/textadventure)
+
 ## Instalación
 
 Para descargar este proyecto necesitas correr el siguiente comando en tu terminal
@@ -18,4 +20,16 @@ Los requisitos para poder utilizar este repositorio son:
 * Git
 * IDE de tu preferencia
   
-  
+## Objetivos
+Los objetivos de este juego se enumeran a continuación y se muestra su progreso:
+- [x] Hacer core mechanics
+- [x] Finalizar el game loop
+- [x] Crear versión web
+- [x] Publicar el juego
+- [x] Compartir el juego
+
+### Autores
+*Katia Kavanagh - Programadora - [@y2kat](https://github.com/y2kat)
+
+## Objetivos
+Aprender las bases de TextMeshPro en Unity
